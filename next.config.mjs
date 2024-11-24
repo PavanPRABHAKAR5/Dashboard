8e3e05268cc42a6662b38f67128fd4b48b85b7dd
PavanPRABHAKAR5 <pavanprabhakar4@gmail.com>
2024-11-25 02:45:17 +0530

    build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverExternalPackages: ["mongoose"],
-      },
+
       eslint:{
         ignoreDuringBuilds: true,
       }
