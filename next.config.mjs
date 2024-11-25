7eb443ced03037cab21c32a4d4048ce95c7689ed
PavanPRABHAKAR5 <pavanprabhakar4@gmail.com>
2024-11-25 17:09:36 +0530

    deploy-invoice

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		serverActions: true
-	},
-      eslint:{
-        ignoreDuringBuilds: true,
-      }
 };
 
 export default nextConfig;
