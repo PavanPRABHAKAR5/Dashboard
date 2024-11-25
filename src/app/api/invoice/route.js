259159321b42efc832fb3c261aedf1ebc049bc61
PavanPRABHAKAR5 <pavanprabhakar4@gmail.com>
2024-11-25 17:34:03 +0530

    fetchInvoiceApi

--- src/app/api/invoice/route.js
+++ src/app/api/invoice/route.js
@@ -19,13 +19,19 @@ export async function POST(req, res){
     }
 }
 
-export async function GET(req, res){
-    try{
+const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+};
+
+export async function GET() {
+    try {
         await dbConfig();
         const invoices = await Invoice.find({}).sort({ createdAt: -1 });
-        return NextResponse.json(invoices, {status: 200});
-    }catch(err){
-        console.log(err);
-        return NextResponse.json({message: 'Failed to fetch invoices'}, {status: 500});
+        return NextResponse.json(invoices, { headers, status: 200 });
+    } catch (err) {
+        console.error("Full error details:", err);
+        return NextResponse.json({ error: err.message || "Internal Server Error" }, { headers, status: 500 });
     }
 }
